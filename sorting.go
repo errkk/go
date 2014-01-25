a7bf3269f5eba90e4edac712993f06e808f97cef
Eric George <eric@pokelondon.com>
2014-01-25 17:58:26 +0000

    Sorting

--- sorting.go
+++ sorting.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 import "sort"
 
+// Custom sorting
+type ByLength []string
+
+func (s ByLength) Len() int {
+    return len(s)
+}
+func (s ByLength) Swap(i, j int) {
+    s[i], s[j] = s[j], s[i]
+}
+func (s ByLength) Less(i, j int) bool {
+    return len(s[i]) < len(s[j])
+}
+
 func main() {
     strs := []string{"c", "a", "b"}
     // Sorting mutates original and doesnt return a copy
@@ -15,4 +28,13 @@ func main() {
 
     s := sort.IntsAreSorted(ints)
     fmt.Println("Sorted: ", s)
+
+    // Custom sort
+    // By following this same pattern of creating a custom type,
+    // implementing the three Interface methods on that type,
+    // and then calling sort.Sort on a collection of that custom type,
+    // we can sort Go slices by arbitrary functions.
+    fruits := []string{"peach", "banana", "kiwi"}
+    sort.Sort(ByLength(fruits))
+    fmt.Println(fruits)
 }
